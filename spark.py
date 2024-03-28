b591c995be20c51eacd2a72eb645b9e556a8e073
Sugeng D. Cahyo <34049790+sugengdcahyo@users.noreply.github.com>
2024-03-29 04:52:25 +0700

    Update jupyterlab and spark config

--- spark.py
+++ spark.py
@@ -3,21 +3,21 @@ from pyspark.sql import SparkSession
 # Inisialisasi SparkSession dengan akses ke Hadoop HDFS
 spark = SparkSession.builder \
     .appName("Read CSV Column from Hadoop") \
-    .config("spark.hadoop.fs.defaultFS", "hdfs://hadoop:8020") \
+    .config("spark.hadoop.fs.defaultFS", "hdfs://hadoop:9820") \
     .getOrCreate()
 
 # Lokasi file CSV di HDFS
-csv_path = "hdfs://hadoop:8020/sheet/Tweet.csv"
+csv_path = "hdfs://hadoop:9820/sheet/Tweet.csv"
 
-# Membaca file CSV dari HDFS
-df = spark.read.csv(csv_path, header=True, inferSchema=True)
+print(csv_path)
+# # Membaca file CSV dari HDFS
+# df = spark.read.csv(csv_path, header=True, inferSchema=True)
 
-print(df)
-# Memilih kolom yang diinginkan. Misalnya, kita ingin membaca kolom 'name' dan 'age'
-selected_columns = df.select("name", "age")
+# # Memilih kolom yang diinginkan. Misalnya, kita ingin membaca kolom 'name' dan 'age'
+# selected_columns = df.select("name", "age")
 
-# Menampilkan data dari kolom yang dipilih
-selected_columns.show()
+# # Menampilkan data dari kolom yang dipilih
+# selected_columns.show()
 
-# Jangan lupa untuk menghentikan SparkSession setelah selesai
-spark.stop()
+# # Jangan lupa untuk menghentikan SparkSession setelah selesai
+# spark.stop()
